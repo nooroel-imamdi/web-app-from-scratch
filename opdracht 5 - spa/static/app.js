73f645ac2f4a68bcfd76824c58ea1c1717274f7e
Nooroel Imamdi <nooroelimamdi@gmail.com>
2017-02-09 10:56:30 +0100

    Opdracht 5 - JavaScript-onderdeel

---
+++ opdracht 5 - spa/static/app.js
@@ -0,0 +1,36 @@
+'use strict';
+(function (){
+	var app = {
+		init: function(){
+			routes.init();
+			sections.toggle("#start-screen");
+		}	
+	};
+
+	var routes = {
+		init: function(){
+			window.addEventListener("hashchange",function(){
+				sections.toggle(location.hash);
+			});
+		}	
+	};
+
+	var sections ={
+		toggle: function(route){
+			console.log(route);
+			var activeSection = document.querySelector(route);
+			var viewSelections = document.getElementsByClassName('view');
+
+			for (var i = 0; i < viewSelections.length; i++) {
+			 	if(viewSelections[i].id == activeSection.id){
+			 		viewSelections[i].classList.remove('hide');
+			 	} else {
+			 		viewSelections[i].classList.add('hide');
+			 	}
+			} 
+		},
+	};
+
+	app.init();
+
+})();

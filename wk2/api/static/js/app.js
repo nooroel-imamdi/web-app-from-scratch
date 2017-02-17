d857002e1ac50bccfbdc9dfa94f4b6822fba525a
Rob van der Pas <rvdpas94@gmail.com>
2017-02-17 12:54:40 +0100

    Remove unused code lines, makes it messy
    Detailpage isn't working in your live version either

--- wk2/api/static/js/app.js
+++ wk2/api/static/js/app.js
@@ -4,30 +4,15 @@
 	var app = {
 		init: function(){
 			routes.init();
-			// sections.toggle("");
 			dataRijksmuseum.init();
 			search.init();
-			// renderHTML.init();
 			renderOverview.init();
 			renderDetail.init();
-
 		}	
 	};
-	// routie({
-	// 	'': function() {
-	//       console.log("screw you guys, I'm going");
-	//     },
-	//     '#nooroel': function() {
-	//       console.log("Ja hoor");
-	//     },
-
-	// });
 
 	var routes = {
 		init: function(){
-			// window.addEventListener("hashchange",function(){
-			// 	sections.toggle(location.hash);
-			// });
 			routie({
 				'': function() {
 				    routie('zoeken');
@@ -46,20 +31,7 @@
 			});
 		}
 	};
-	// var sections = {
-	// 	toggle: function(route){
-	// 		var activeSection = document.querySelector(route);
-	// 		var showSection = document.getElementsByClassName('show');
-
-	// 		for (var i = 0; i < showSection.length; i++) {
-	// 		 	if(showSection[i].id == activeSection.id){
-	// 		 		showSection[i].classList.remove('hide');
-	// 		 	} else {
-	// 		 		showSection[i].classList.add('hide');
-	// 		 	}
-	// 		} 
-	// 	},
-	// };
+	
 	var search = {
 		init: function(){
 			document.getElementById("search-btn").addEventListener('click', this.userInput);
@@ -67,10 +39,10 @@
 		userInput: function(){
 			var query = document.getElementById("user-input").value;
 			dataRijksmuseum.init(query);
-			// renderHTML.init(query);
 			renderOverview.init(query);
 		}
 	};
+	
 	var dataRijksmuseum = {
 		init: function(query){
 			var request = new window.XMLHttpRequest();
@@ -78,43 +50,28 @@
 			console.log(query);
 			var url = "https://www.rijksmuseum.nl/api/nl/collection?q=" + userInput + "&key=NG2q9L0R&format=json";
 			request.open("GET", url, true);
-			// request.open("GET", "https://www.rijksmuseum.nl/api/nl/collection?&key=NG2q9L0R&format=json", true);
 			request.onload = function() {
 			   if (request.status >= 200 && request.status < 400) {
 			       // Success!
 			       var data = JSON.parse(request.responseText);
-			       // renderHTML.init(data);
 			       renderOverview.init(data);
 			       renderDetail.init(data);
 			   } else {
-			       // We reached our target server, but it returned an error
+			       console.log("Something should happen here, or else it's unnecessary.")
 			   }
-			   // console.log(data);
+
 
 			};
 
 			request.onerror = function() {
-			   // There was a connection error of some sort
+			   console.log("Something should happen here, or else it's unnecessary.");
 			};
 
 			request.send();
 
 		},
 	};
-	// var renderHTML = {
-	// 	init: function(data){
-	// 		var rawTemplating = document.getElementById("art-template").innerHTML;
-	// 		var compiledTemplate = Handlebars.compile(rawTemplating);
-	// 		var ourGeneratedHTML = compiledTemplate(data);
-
-	// 		var outputArt = document.getElementById("output-art");
-	// 		outputArt.innerHTML = ourGeneratedHTML;
-
-	// 		// console.log(data)
-
-	// 		// document.getElementById('art-template').innerHTML = Handlebars.compile(document.getElementById('output-art').innerHTML)(data);
-	// 	}
-	// };
+	
 	var renderOverview = {
 		init: function(data){
 			var rawTemplating = document.getElementById("overview-template").innerHTML;
@@ -125,6 +82,7 @@
 			outputArt.innerHTML = ourGeneratedHTML;
 		}
 	};
+	
 	var renderDetail = {
 		init: function(data){
 			var rawTemplating = document.getElementById("detail-template").innerHTML;
@@ -135,10 +93,9 @@
 			outputArt.innerHTML = ourGeneratedHTML;
 		}
 	};
+	
 	var buildUrl = function(type, id) {
 		var queryUrl = "";
-
-		
 	};
 
 	app.init();
